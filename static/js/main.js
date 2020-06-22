e8ebef86ad5cdca68fa87b288fc07b8942d054b2
ajibola ojo <jibola4jesus@gmail.com>
2020-06-22 13:41:26 +0100

    Add scroll functionality

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,25 @@
 $(() => {
   (() => {
-    // Set Progress bars
+    var headerHeight = $("nav.navbar").outerHeight(); // Target your header navigation here
+
+    $("nav li a").click(function (e) {
+      $(".collapse").collapse("hide");
+      $("nav li").removeClass("active");
+      $(this).parent().addClass("active");
+      var targetHref = $(this).attr("href");
+
+      $("html, body").animate(
+        {
+          scrollTop: $(targetHref).offset().top - headerHeight, // Add it to the calculation here
+        },
+        1000
+      );
+
+      e.preventDefault();
+    });
+  })();
+  // Set Progress bars
+  (() => {
     $(".colored-progress-section").on("click", function () {
       $(this).css("width", $(this).attr("data-value"));
       $(this).css("background-color", $(this).attr("data-color"));
